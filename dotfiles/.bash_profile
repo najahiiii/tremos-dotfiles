41972266cbe32acc51471b2f1b81fba0eecfa4b5
Ahmad Thoriq Najahi <najahi@chips-projects.xyz>
2020-07-24 06:04:11 +0700

    Update
    Signed-off-by: Ahmad Thoriq Najahi <najahi@zephyrus.id>

--- dotfiles/.bash_profile
+++ dotfiles/.bash_profile
@@ -4,11 +4,21 @@
 export HOME PREFIX TMPDIR
 
 export LOCAL_PREFIX="$HOME/.local"
-export PATH="$LOCAL_PREFIX/bin:$HOME/bin:$PREFIX/bin:$PREFIX/bin/applets"
+export PATH="$LOCAL_PREFIX/bin:$HOME/bin:$PREFIX/bin:$PREFIX/bin/applets:$PATH"
 export EDITOR="$PREFIX/bin/nano"
 
 # Required for gpg sign
 export GPG_TTY="$(tty)"
+
+# Python c_Cache
+export PYTHONPYCACHEPREFIX="$HOME/.cache/cpython/"
+
+# Neoftech
+clear && fastfetch
+
+# Aliases
+alias scon="ssh -i $HOME/storage/shared/.ssh/najahi ${1}"
+alias sfcon="sftp -i $HOME/storage/shared/.ssh/najahi ${1}"
 
 ## Load bashrc if shell is interactive.
 if [[ "$-" == *"i"* ]]; then
@@ -16,3 +26,24 @@ if [[ "$-" == *"i"* ]]; then
 		. "${HOME}"/.bashrc
 	fi
 fi
+
+# User
+export USER="najahi"
+
+# Relog
+alias relog="source $HOME/.bash_profile"
+
+# base64 -d
+function dec() {
+    read -p "Input: " url
+    export out=$(echo ${url} | sed 's:.*url=::' | sed 's#&.*##g' | base64 -d)
+    echo 'Decrypted URL:' $out
+    unset url
+}
+
+function decshortid() {
+    read -p "Input: " url
+    export out=$(echo ${url} | sed 's:.*shortid=::' | sed 's#&.*##g' | base64 -d)
+    echo 'Decrypted URL:' $out
+    unset url
+}
--- setup.sh
+++ setup.sh
@@ -7,44 +7,40 @@ HOME="/data/data/com.termux/files/home"
 PREFIX="/data/data/com.termux/files/usr"
 
 if [ "$(uname -o)" != "Android" ]; then
-	echo "This script must be executed within Termux !"
+	echo -e "This script must be executed within Termux !"
 	exit 1
 fi
 
-echo
-echo "Installing configuration files and scripts will"
-echo "remove existing ones. Directories like '~/.config'"
-echo "or '~/.local' will be completely erased and replaced."
-echo
+echo -e "Installing configuration files and scripts will"
+echo -e "remove existing ones. Directories like '~/.config'"
+echo -e "or '~/.local' will be completely erased and replaced."
 read -re -p "Do you want to proceed ? (y/n): " CHOICE
 
 if [[ "$CHOICE" != [Yy] ]]; then
-	echo "Aborting !"
+	echo -e "Aborting !"
 	exit 1
 fi
 
 if [ -f "$SCRIPT_DIR/packages.txt" ]; then
-	echo
-	echo "[@] Installing necessary packages:"
+	echo -e "[@] Installing necessary packages:"
 	pkg install -y $(cat "$SCRIPT_DIR/packages.txt" | grep -vP '^(?:\s+)?#')
 fi
 
 # Just replace objects in $HOME.
 if [ -d "$SCRIPT_DIR/dotfiles" ]; then
-	echo
-	echo "[@] Writing dotfiles for \$HOME:"
+	echo -e "[@] Writing dotfiles for \$HOME:"
 	while IFS= read -r -d '' file; do
 		dst_file_path="${HOME}/$(basename "$file")"
 
 		if [ "$(basename "$file")" = ".gnupg" ] && [ -d "$dst_file_path" ]; then
-			echo "Not overwriting existing '.gnupg' directory..."
+			echo -e "Not overwriting existing '.gnupg' directory..."
 			continue
 		fi
 
 		if [ -d "$file" ]; then
-			echo "Installing directory '\${HOME}/$(basename "$file")'..."
+			echo -e "Installing directory '\${HOME}/$(basename "$file")'..."
 		else
-			echo "Installing file '\${HOME}/$(basename "$file")'..."
+			echo -e "Installing file '\${HOME}/$(basename "$file")'..."
 		fi
 
 		rm -rf "$dst_file_path"
@@ -55,12 +51,11 @@ fi
 
 # Files in $PREFIX should be handled carefully.
 if [ -d "$SCRIPT_DIR/prefix-files" ]; then
-	echo
-	echo "[@] Writing dotfiles for \$PREFIX:"
+	echo -e "[@] Writing dotfiles for \$PREFIX:"
 	cd "$SCRIPT_DIR/prefix-files" && {
 		while IFS= read -r -d '' file; do
 			file=$(echo "$file" | sed 's/^\.\///')
-			echo "Installing file '\${PREFIX}/${file}'..."
+			echo -e "Installing file '\${PREFIX}/${file}'..."
 			mkdir -p "${PREFIX}/$(dirname "$file")"
 			cp -f "$file" "${PREFIX}/${file}"
 		done < <(find . -type f -print0)
@@ -69,9 +64,11 @@ if [ -d "$SCRIPT_DIR/prefix-files" ]; then
 fi
 
 # Request permission for shared storage access.
-termux-setup-storage
+# termux-setup-storage
 
-echo
-echo "Done! Now you need to restart Termux."
-sleep 1
+# Final touch.
+echo -e "Done!"
+for i in {10..1}; do
+	echo -n "$i." && sleep 1;
+done
 am stopservice com.termux/.app.TermuxService
